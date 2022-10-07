1512292b979e1a2a9889ddd7bd28927d391ac2f4
builtbyjosh <pc.refuge@gmail.com>
2022-10-06 22:19:22 -0500

    refactor getTaskById to validate data

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { uuid } from 'uuidv4';
@@ -29,7 +29,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
   }
 
   createTask(createTaskDTO: CreateTaskDto): Task {
